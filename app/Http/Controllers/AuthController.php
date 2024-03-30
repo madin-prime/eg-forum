6b2f5791a2cf45be376d001f50a2b54049d6d489
Nafiu Hasan Madin <madin@egitsoft.com>
2024-03-30 11:52:55 +0600

    feat: add users authentication and users posting system

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Validator;
 
@@ -10,33 +11,56 @@ class AuthController extends Controller
 {
     public function register(Request $request){
         $validator = Validator::make($request->all(), [
+            'username'=> 'required|unique:users',
             'name'=> 'required',
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users',
             'password' => 'required|min:8'
          ]);
 
+         $validator->setCustomMessages([
+            'username.unique' => 'A user with that username already exists!',
+            'email.unique' => 'A user with that email already exists!'
+          ]);
+
          if($validator->fails()){
-            return 'Error';
+            return $validator->errors()->all();
          }
-         
+
          User::create([
             'name'=> $request->name,
             'email'=> $request->email,
             'password'=> bcrypt($request->password),
+            'username'=> $request->username,
          ]);
 
-         return view('pages.auth.login');
+         return redirect()->route('login');
     }
 
     public function login(Request $request){
       $auth = auth()->attempt($request->only('password','email'));
 
       if($auth){
-         return redirect()->to('home');
+         return redirect()->route('home');
       }
 
       else{
          return 'Wrong User';
       }
+    }
+
+    public function logout(){
+      auth()->logout();
+
+      return redirect()->route('login');
+
+    }
+
+    public function deleteAccount(Request $request){
+
+      $user = $request->user();
+      $user->delete();
+
+
+      return redirect()->route('login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,22 +16,34 @@ use Illuminate\Http\Request;
 |
 */
 Route::middleware('guest')->group(function () {
+
     Route::get('/', function () {
         return view('pages.auth.login');
     })->name('login');
+
     Route::post('/',[AuthController::class, 'login'])->name('log-in');
+
     Route::post('/register', [AuthController::class , 'register'])->name('auth');
+
     Route::get('/register', function () {
         return view('pages.auth.register');
     })->name('register');
+
 });
 
 Route::middleware('auth')->group(function () {
+
+    Route::get('/post', function (Request $request) {
+        return view('pages.post')->with('user', $request->user());
+    })->name('post');
     
     Route::get('/home', function (Request $request) {
-        return view('pages.home')->with('user', $request->user());
-    })->name('name');
+        return view('pages.home')->with('user', $request->user())->with('posts', Post::all());
+    })->name('home');
+
+    Route::post('/post', [ PostController::class , 'createPost' ])->name('post-upload');
+
+    Route::get('logout', [ AuthController::class, 'logout'])->name('logout');
+    
+    Route::get('delete-account', [AuthController::class, 'deleteAccount'])->name('delete');
 });
-
-
-Route::get('/test', fn(Request $request) => $request->user());
